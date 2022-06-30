da4ef577a5c26425761ed4ada0ba55d807832022
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-06-30 21:22:56 +0600

    Change __all__ type from List to Tuple

--- source/builders/__init__.py
+++ source/builders/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['numbered_list', 'build_embed', 'build_enroll_view']
+__all__ = ("numbered_list", "build_lfg_embed", "build_enroll_view", "notify_main", "notify_reserve")
 
 
-from .embed import numbered_list, build_embed
+from .embed import numbered_list, build_lfg_embed, notify_main, notify_reserve
 from .enroll import build_enroll_view
